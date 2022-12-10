e0b838c399263a56c0c1b6c32be0b6cb92ef053f
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2022-12-10 00:11:39 -0400

    feat: Show articles

---
+++ app/Article.tsx
@@ -0,0 +1,11 @@
+type Props = {
+    article: Article
+}
+
+function Article({ article }: Props) {
+  return (
+    <div>Article</div>
+  )
+}
+
+export default Article
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,18 @@
 import { categories } from "../constants";
 import fetchNews from "../lib/fetchNews";
+import NewsList from "./NewsList";
 
 async function Homepage() {
   // fetch the news data
-
   const news: NewsResponse = await fetchNews(categories.join(','));
-
-  console.log(news);
-
   // console.log(news);
+
   
   return (
     <div>
-      {/* NewsList news */}
+      <NewsList
+        news={news}
+      />
     </div>
   )
 }
