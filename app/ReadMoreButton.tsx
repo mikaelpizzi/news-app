cf3d28c11933b2e8b55671a0efdbfc8f53a9d066
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2022-12-21 16:59:02 -0400

    fix: Fix dynamic routing for production
    Fix dynamic routing for production because it was not working because it's supposed to be a dynamic page. In nextjs13, all dynamic pages should have []. So under the article folder another folder called [article] was created and the page.tsx was throw in there.

--- app/ReadMoreButton.tsx
+++ app/ReadMoreButton.tsx
@@ -17,14 +17,14 @@ function ReadMoreButton({article}: Props) {
 
         var finalQueryString = queryString.replace(/#/g, "");
 
-        const url = `/article?${finalQueryString}`;
+        const url = `/article/article?${finalQueryString}`;
         router.push(url);
     }
 
   return (
     <button
         onClick={handleClick}
-        className="bg-orange-400 h-10 rounded-b-lg dark:text-gray-900 hover:bg-orange-500 hidden"
+        className="bg-orange-400 h-10 rounded-b-lg dark:text-gray-900 hover:bg-orange-500"
     >Read More</button>
   )
 }
